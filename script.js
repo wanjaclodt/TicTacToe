603c39b5c79adb15f98a67ffc03498830f1b8e5b
wXclode <wanjaclodt@outlook.de>
2023-07-13 19:46:18 +0200

    Implemented player switching functionality in 'addSign()' function for alternating between 'X' and 'O'

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+var currentPlayer = "X"; // Starting player
+
 function addSign(btn) {
     var button = document.getElementById(btn);
     if (button.innerHTML === "") {
-        button.innerHTML = "X";
+        button.innerHTML = currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
 }
